02d0e0061848e114d8d0b67a8cbeea57965eabeb
Tomasz Filo Zegarlicki <tzegarlicki@gmail.com>
2019-09-29 13:11:53 +0200

    x

---
+++ src/main/java/filoPL/AlbumServlet.java
@@ -0,0 +1,33 @@
+package filoPL;
+
+import java.io.Serializable;
+
+public class AlbumServlet implements Serializable {
+    private String title;
+    private String author;
+    private int year;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
